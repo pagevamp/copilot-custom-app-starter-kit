d2d8e5254285ac5dd8ccf9a85c9965b54ff2a4b6
Sajan Rajbhandari <sazanrjb@gmail.com>
2024-01-16 13:41:08 +0545

    Copilot SDK setup (#1)
    * Set up copilot SDK
    
    * Remove webhook signing secret
    
    * Update readme
    
    * Database client set up
    
    * Update readme

---
+++ src/lib/db.ts
@@ -0,0 +1,17 @@
+import { PrismaClient } from '@prisma/client';
+
+class DBClient {
+  private static client: PrismaClient;
+
+  static getInstance(): PrismaClient {
+    if (this.client) {
+      return this.client;
+    }
+
+    this.client = new PrismaClient();
+
+    return this.client;
+  }
+}
+
+export default DBClient;
